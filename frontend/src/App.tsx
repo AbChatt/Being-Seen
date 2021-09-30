f74f5d72fadfd8f2825e4c3656040f8e92bd65d1
Joshua Daniel <joshuajadaniel@gmail.com>
2021-09-30 14:12:51 -0400

    TEA4-23: add routing for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,31 @@
-const App = () => <h1>Hello, World!</h1>;
+import {
+  Route,
+  Switch,
+  Redirect,
+  BrowserRouter as Router,
+} from "react-router-dom";
+
+import HomePage from "pages/Home";
+import LoginPage from "pages/Login";
+import SignupPage from "pages/Signup";
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route exact path="/login">
+        <LoginPage />
+      </Route>
+      <Route exact path="/signup">
+        <SignupPage />
+      </Route>
+      <Route path="*">
+        <Redirect to="/" />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
