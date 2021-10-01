26089aecdc5187cb603602b3f333e1bf5fd6c290
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-01 18:18:13 -0400

    TEA4-23: utilize drawer and navbar menus for header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,40 +1,56 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import Box from "@mui/material/Box";
+import MuiLink from "@mui/material/Link";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
-import MuiLink from "@mui/material/Link";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
+import Drawer from "components/Menu/Drawer";
+import Navbar from "components/Menu/Navbar";
+
+import { removeAuthToken } from "utils/checkAuth";
 import styles from "./Header.module.scss";
 
-const Header = () => (
-  <AppBar position="sticky">
-    <Toolbar className={styles.wrapper}>
-      <MuiLink to="/" component={Link} className={styles.logoLink}>
-        <img src="https://dummyimage.com/128x128&text=Logo" alt="Being Seen" />
-      </MuiLink>
-      <div>
-        <Button
-          to="/login"
-          component={Link}
-          variant="outlined"
-          color="inherit"
-          className={styles.button}
-        >
-          Login
-        </Button>
-        <Button
-          to="/signup"
-          component={Link}
-          variant="outlined"
-          color="inherit"
-          className={styles.button}
-        >
-          Signup
-        </Button>
-      </div>
-    </Toolbar>
-  </AppBar>
-);
+const Header = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const closeDrawer = () => setDrawerOpen(false);
+  const openDrawer = () => setDrawerOpen(true);
+
+  const handleLogout = () => {
+    removeAuthToken();
+    window.location.reload();
+  };
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar className={styles.wrapper}>
+        <MuiLink to="/" component={Link} className={styles.logoLink}>
+          <img
+            src="https://dummyimage.com/320x128&text=Logo"
+            alt="Being Seen"
+          />
+        </MuiLink>
+        <div>
+          <Box sx={{ display: { xs: "none", md: "block" } }}>
+            <Navbar onLogout={handleLogout} />
+          </Box>
+          <Box sx={{ display: { xs: "block", md: "none" } }}>
+            <IconButton onClick={openDrawer} color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Drawer
+              open={drawerOpen}
+              onClose={closeDrawer}
+              onLogout={handleLogout}
+            />
+          </Box>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
