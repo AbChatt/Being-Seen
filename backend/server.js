1a0b5a635f3acd34e8db82cea1cae4bf431bf3c7
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-02 22:43:29 -0400

    TEA4-30: create and add request logger to application

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 
+import requestLogger from "./middleware/requestLogger.js";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -9,6 +11,7 @@ const API_VERSION = process.env.API_VERSION;
 app.get(`api/${API_VERSION}/`, (req, res) => {
   res.send("Hello, World!");
 });
+app.use("/", requestLogger);
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
