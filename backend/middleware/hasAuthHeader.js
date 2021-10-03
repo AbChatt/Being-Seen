31c36fb83b7c1f9872dbeca190efa271d384be85
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-03 00:35:12 -0400

    TEA4-30: create middleware for authorization header

---
+++ backend/middleware/hasAuthHeader.js
@@ -0,0 +1,13 @@
+import { StatusCodes } from "http-status-codes";
+import { createTextMessage } from "../utils/defaultMessages.js";
+
+const hasAuthHeader = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .send(createTextMessage("No authorization header found"));
+  }
+  next();
+};
+
+export default hasAuthHeader;
