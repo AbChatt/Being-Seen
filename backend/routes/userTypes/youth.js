e497d9ea90ffef197e89b2a4a74a1c60d46f36a2
CharlesXu123 <78982364+CharlesXu123@users.noreply.github.com>
2021-10-05 15:16:35 -0400

    TEA4-35: add userType/youth.js

--- backend/routes/userTypes/youth.js
+++ backend/routes/userTypes/youth.js
@@ -1,9 +1,50 @@
 import express from "express";
+import { StatusCodes } from "http-status-codes";
+
+import validateDonorSignup from "../../middleware/validateDonorSignup.js";
+
+import {
+  createTextMessage,
+  createJwtMessage,
+} from "../../utils/defaultMessages.js";
+import { createUserToken } from "../../utils/jwtHelpers.js";
+import userRoles from "../../utils/userRoles.js";
+
+import Youth from "../../models/Youth.js";
+import User from "../../models/User.js";
 
 const router = express.Router();
+const validateUserSignup = validateDonorSignup;
 
-router.get("/", () => {
-  res.send("Youth endpoints here");
+router.use("/signup", validateUserSignup);
+router.post("/signup", async (req, res) => {
+  const newUser = new User({
+    role: userRoles.youth,
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  const newYouth = new Youth({
+    name: req.body.name,
+    username: req.body.username,
+    date_of_birth: req.body.date_of_birth,
+    profile_picture: req.body.profile_picture || "#",
+    story: req.body.story,
+    saving_plan: req.body.saving_plan
+  });
+
+  try {
+    await newUser.save();
+    await newYouth.save();
+    const jwtToken = createUserToken(req.body.username, userRoles.youth);
+    res.status(StatusCodes.CREATED).send(createJwtMessage(jwtToken));
+  } catch (err) {
+    console.log(err);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(createTextMessage("Error saving donor to database"));
+  }
 });
 
 export default router;
+
