0f44324ae11d1af78435232b8b406cb363985af8
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-05 10:52:38 -0400

    TEA4-32: modification to request logger to use moment package

--- backend/middleware/requestLogger.js
+++ backend/middleware/requestLogger.js
@@ -1,27 +1,12 @@
 import chalk from "chalk";
-
-const getCurrentDate = () => {
-  const currentDate = new Date();
-  return (
-    currentDate.getFullYear() +
-    "-" +
-    (currentDate.getMonth() + 1) +
-    "-" +
-    currentDate.getDate() +
-    " " +
-    currentDate.getHours() +
-    ":" +
-    currentDate.getMinutes() +
-    ":" +
-    currentDate.getSeconds()
-  );
-};
+import moment from "moment";
 
 const requestLogger = (req, res, next) => {
-  const date = chalk.blue(getCurrentDate());
+  const currentDate = moment(moment.now()).format("YYYY-MM-DD hh:mm:ss");
+  const requestDate = chalk.blue(currentDate);
   const requestUrl = chalk.cyanBright(req.url);
   const requestMethod = chalk.magenta(req.method);
-  console.log(`[${date}] ${requestMethod}:${requestUrl}`);
+  console.log(`[${requestDate}] ${requestMethod}:${requestUrl}`);
   next();
 };
 
