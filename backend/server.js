37c15654f274d05dc7aefa6d05e9f67a3a25fe50
Tony-beeper <tyx1999120755@gmail.com>
2021-10-04 21:35:40 -0400

    TEA4-32: Body-Parser and connect to Mongo from server

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,33 @@ import dotenv from "dotenv";
 import express from "express";
 import chalk from "chalk";
 
+import mongoose from "mongoose";
 import userRoute from "./routes/user.js";
 import requestLogger from "./middleware/requestLogger.js";
 
 dotenv.config();
 const app = express();
+
+//body-parser from express
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 const PORT = process.env.PORT;
 const API_VERSION = process.env.API_VERSION;
 
 app.use("/", requestLogger);
 app.use(`/api/${API_VERSION}/user`, userRoute);
+
+
+//Connected To DB
+mongoose.connect(process.env.MONGO_CONN_STR, { useNewUrlParser: true }, () =>
+  console.log("connected to DB!")
+);
+
 
 app.listen(PORT, () => {
   console.log(chalk.whiteBright(`Listening at http://localhost:${PORT}`));
