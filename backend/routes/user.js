ee306ef5b5da2717ea7c38c4c02af6cee1a05581
Tony-beeper <tyx1999120755@gmail.com>
2021-10-04 21:38:43 -0400

    TEA4-32: Register as Donor
    a user account will be signed up along with donor account

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,8 @@ import { decodeUserToken } from "../utils/jwtHelpers.js";
 import { createTextMessage } from "../utils/defaultMessages.js";
 import hasAuthHeader from "../middleware/hasAuthHeader.js";
 
+import Donor from "../models/donors.js";
+import User from "../models/users.js"
 const router = express.Router();
 
 router.use("/validate", hasAuthHeader);
@@ -14,6 +16,36 @@ router.get("/validate", (req, res) => {
     res
       .status(StatusCodes.UNAUTHORIZED)
       .send(createTextMessage("JWT passed is not valid"));
+  }
+});
+
+router.post("/signup/donors", async (req, res) => {
+  console.log(req.body);
+  const user = new User({
+    username: req.body.username,
+    role: "donors",
+  })
+
+  const donor = new Donor({
+    name: req.body.name,
+    username: req.body.username,
+    profile_picture: req.body.profile_picture,
+    date_of_birth: req.body.date_of_birth,
+    password: req.body.password,
+    credit_card: req.body.credit_card,
+    organization: req.body.organization,
+  });
+  try {
+    const savedDonor = await donor.save();
+    res.json(savedDonor);
+  } catch (err) {
+    res.json({ message: err });
+  }
+  try {
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json({ message: err });
   }
 });
 
