a84a235aeefd54c14eb36b85ef20bf82885845ca
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-05 15:42:36 -0400

    TEA4-35: clean up database schema

--- backend/models/Youth.js
+++ backend/models/Youth.js
@@ -1,37 +1,30 @@
 import mongoose from "mongoose";
 
-//
-const Schema = mongoose.Schema;
-
-const YouthSchema = new Schema({
+const YouthSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-
   username: {
     type: String,
     required: true,
-  },
-
-  profile_picture: {
-    type: String,
-    default: "abc balaba",
   },
   date_of_birth: {
     type: String,
     required: true,
   },
-  story:{
-      type: String,
-      default: "enter your story here",
+  profile_picture: {
+    type: String,
+    required: true,
   },
-  saving_plan:{
-      type: String,
-      default: "enter your saving plan here"
-  }
+  story: {
+    type: String,
+    default: "This user has not published their story",
+  },
+  saving_plan: {
+    type: String,
+    default: "This user has not published their saving plan",
+  },
 });
-
-//module.exports = mongoose.model("youth", Youthschema)
 
 export default mongoose.model("youths", YouthSchema);
