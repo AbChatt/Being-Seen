8d8602739b2b7140565ab786714b8a889516a76b
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-05 01:15:21 -0400

    TEA4-32: formatting server.js

--- backend/server.js
+++ backend/server.js
@@ -8,27 +8,21 @@ import requestLogger from "./middleware/requestLogger.js";
 
 dotenv.config();
 const app = express();
-
-//body-parser from express
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(express.json());
-
 const PORT = process.env.PORT;
 const API_VERSION = process.env.API_VERSION;
 
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// App routes
 app.use("/", requestLogger);
 app.use(`/api/${API_VERSION}/user`, userRoute);
 
-
-//Connected To DB
+// Mongo database connection
 mongoose.connect(process.env.MONGO_CONN_STR, { useNewUrlParser: true }, () =>
   console.log("connected to DB!")
 );
-
 
 app.listen(PORT, () => {
   console.log(chalk.whiteBright(`Listening at http://localhost:${PORT}`));
