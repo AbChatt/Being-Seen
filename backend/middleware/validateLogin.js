d1f25aac4d7ca8d16d42c57ff5742f82300326e8
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-05 13:27:40 -0400

    TEA4-24: create middleware for validating whether body is properly formed

---
+++ backend/middleware/validateLogin.js
@@ -0,0 +1,22 @@
+import { StatusCodes } from "http-status-codes";
+import { createTextMessage } from "../utils/defaultMessages.js";
+
+const validateLogin = (req, res, next) => {
+  // Validate username
+  if (!req.body.username) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("Username field is empty"));
+  }
+
+  // Validate password
+  if (!req.body.password) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("Password field is empty"));
+  }
+
+  next();
+};
+
+export default validateLogin;
