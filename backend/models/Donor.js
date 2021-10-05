5a4f62066de463bccfd648953b1142f677cca695
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-05 08:19:22 -0400

    TEA4-32: clean up mongo schema/model for users and donors

--- backend/models/Donor.js
+++ backend/models/Donor.js
@@ -1,42 +1,26 @@
 import mongoose from "mongoose";
 
-//
-const Schema = mongoose.Schema;
-
-const DonorSchema = new Schema({
+const DonorSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-
   username: {
     type: String,
     required: true,
-  },
-
-  profile_picture: {
-    type: String,
-    default: "abc balaba",
   },
   date_of_birth: {
     type: String,
     required: true,
   },
-  password: {
-    type: String,
-    required: true,
-  },
-
-  credit_card: {
+  profile_picture: {
     type: String,
     required: true,
   },
   organization: {
     type: String,
-    default: "None",
+    required: true,
   },
 });
-
-//module.exports = mongoose.model("Posts", Postschema)
 
 export default mongoose.model("donors", DonorSchema);
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
-//
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
   },
-
+  password: {
+    type: String,
+    required: true,
+  },
   role: {
     type: String,
     required: true,
   },
 });
-
-//module.exports = mongoose.model("Posts", Postschema)
 
 export default mongoose.model("users", UserSchema);
