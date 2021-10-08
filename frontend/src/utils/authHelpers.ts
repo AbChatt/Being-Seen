a19a6c73e018b0b6af14133c2032a8c864a9fabd
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-08 15:18:59 -0400

    TEA4-42: document authHelpers

--- frontend/src/utils/authHelpers.ts
+++ frontend/src/utils/authHelpers.ts
@@ -3,13 +3,19 @@ import hasOwnProperty from "utils/hasOwnProperty";
 import { AxiosRequestConfig } from "axios";
 import UserRoles from "utils/UserRoles";
 
+// Define key for storing and retrieving a stored JWT token from local storage
 const STORAGE_AUTH_KEY = "auth";
 
+// Define structure of expected JWT tokens (username to identify user and role
+// to identify what user is authorized to do)
 interface UserJwt {
   username: string;
   role: UserRoles;
 }
 
+// Decode JWT token from local storage and return the decoded token provided it
+// can be decoded and it follows the general contract of UserJwt. If not,
+// return null to indicate the user is not authenticated.
 const decodeAuthToken = (): UserJwt | null => {
   let payload: Object = {};
   const authToken = localStorage.getItem(STORAGE_AUTH_KEY) || "";
@@ -37,14 +43,18 @@ const decodeAuthToken = (): UserJwt | null => {
   return null;
 };
 
+// Store a given JWT token into local storage (no checks are performed)
 const setAuthToken = (token: string) => {
   localStorage.setItem(STORAGE_AUTH_KEY, token);
 };
 
+// Remove the stored (or absent) JWT token from local storage
 const removeAuthToken = () => {
   localStorage.removeItem(STORAGE_AUTH_KEY);
 };
 
+// Construct header with bearer authentication scheme using JWT token from
+// local storage (if no JWT token is found, it is constructed as "Bearer null")
 const getAuthHeader = (): AxiosRequestConfig => ({
   headers: {
     Authorization: `Bearer ${localStorage.getItem(STORAGE_AUTH_KEY)}`,
