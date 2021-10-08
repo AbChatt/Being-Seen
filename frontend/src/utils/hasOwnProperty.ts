8f0ef121dfd62754b75018109b63c1fef69072d8
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-08 15:21:08 -0400

    TEA4-42: document hasOwnProperty

--- frontend/src/utils/hasOwnProperty.ts
+++ frontend/src/utils/hasOwnProperty.ts
@@ -1,3 +1,5 @@
+// Check whether a given property exists on a given object (improvement over
+// standard hasOwnProperty() method for TypeScript static checking)
 const hasOwnProperty = <X extends {}, Y extends PropertyKey>(
   obj: X,
   property: Y
