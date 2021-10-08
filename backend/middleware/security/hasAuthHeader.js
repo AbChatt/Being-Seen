b3c278fa19f5f6a751adbd19cd6ad21b42e35278
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-08 15:48:29 -0400

    TEA4-42: document log, login and security middleware

--- backend/middleware/security/hasAuthHeader.js
+++ backend/middleware/security/hasAuthHeader.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import { createTextMessage } from "../../utils/defaultMessages.js";
 
+// Middleware to validate whether an authorization header was passed
 const hasAuthHeader = (req, res, next) => {
   if (!req.headers.authorization) {
     return res
