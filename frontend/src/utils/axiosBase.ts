7cd1e680b2616b706e7f60fb14bce7aa13bfd1c3
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-08 14:58:14 -0400

    TEA4-42: document axios base instance

--- frontend/src/utils/axiosBase.ts
+++ frontend/src/utils/axiosBase.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+// Define a base axios instance with a base URL. This prevents repetition
+// associated with prefixing calls with protocol, domain, path, and version.
 const axiosBase = axios.create({
   baseURL: "http://localhost:5000/api/v1",
 });
