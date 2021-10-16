495c6b3b54823d7299f206761585e6062c55f6f5
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-16 19:58:18 -0400

    TEA4-52: add load in transition based on image loading

--- frontend/src/components/YouthCard/YouthCard.tsx
+++ frontend/src/components/YouthCard/YouthCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import Card from "@mui/material/Card";
@@ -6,6 +7,7 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Avatar from "@mui/material/Avatar";
+import Grow from "@mui/material/Grow";
 
 import styles from "./YouthCard.module.scss";
 
@@ -17,34 +19,46 @@ interface YouthCardProps {
   age: number;
 }
 
-const YouthCard = ({ name, story, username, image, age }: YouthCardProps) => (
-  <Link to={`/u/${username}`} className={styles.linkOverrides}>
-    <Card>
-      <CardActionArea>
-        <Avatar
-          src={image}
-          sx={{ height: 200, width: "100%" }}
-          variant="square"
-        />
-        <CardContent>
-          <Typography variant="h6">
-            {name} ({age})
-          </Typography>
-          <Typography variant="body2" color="primary">
-            @{username}
-          </Typography>
-          <Divider sx={{ mt: 1, mb: 2 }} />
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            className={styles.lineOverflow}
-          >
-            {story}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  </Link>
-);
+const YouthCard = ({ name, story, username, image, age }: YouthCardProps) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Required when initial image onLoad does not fire
+    setTimeout(() => setLoaded(true), 750);
+  }, []);
+
+  return (
+    <Link to={`/u/${username}`} className={styles.linkOverrides}>
+      <Grow in={loaded}>
+        <Card>
+          <CardActionArea>
+            <Avatar
+              src={image}
+              variant="square"
+              sx={{ height: 200, width: "100%" }}
+              imgProps={{ onLoad: () => setLoaded(true) }}
+            />
+            <CardContent>
+              <Typography variant="h6">
+                {name} ({age})
+              </Typography>
+              <Typography variant="body2" color="primary">
+                @{username}
+              </Typography>
+              <Divider sx={{ mt: 1, mb: 2 }} />
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className={styles.lineOverflow}
+              >
+                {story}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grow>
+    </Link>
+  );
+};
 
 export default YouthCard;
