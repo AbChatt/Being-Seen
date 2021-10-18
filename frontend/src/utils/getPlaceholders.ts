dc06949e3136dc85eea1d2a3b9563c2daf541781
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-18 16:38:31 -0400

    TEA4-50: update placeholders to contain an ID to easily distinguish repeated elements

--- frontend/src/utils/getPlaceholders.ts
+++ frontend/src/utils/getPlaceholders.ts
@@ -1,20 +1,21 @@
 import moment from "moment";
 import { Donation, PublicYouth } from "common/Types";
 
-const getDonation = (): Donation => ({
-  donor: "Donald Trump",
-  youth: "Barack Obama",
+const getDonation = (id: number): Donation => ({
+  donor: `Donald Trump #${id}`,
+  youth: `Barack Obama #${id}`,
   image:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/800px-Donald_Trump_official_portrait.jpg",
   date: moment().toISOString(),
   amount: 1200,
 });
 
-const getDonations = (): Donation[] => [...Array(30)].fill(getDonation());
+const getDonations = (): Donation[] =>
+  [...Array(30)].map((v, i) => getDonation(i));
 
-const getPublicYouth = (): PublicYouth => ({
-  name: "Barack Obama",
-  username: "BarackObama",
+const getPublicYouth = (id: number): PublicYouth => ({
+  name: `Barack Obama`,
+  username: `BarackObama${id}`,
   image:
     "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/President_Barack_Obama%2C_2012_portrait_crop.jpg/800px-President_Barack_Obama%2C_2012_portrait_crop.jpg",
   story: `Barack Hussein Obama II is an American politician, author, and retired attorney who served as the 44th president of the United States from 2009 to 2017. A member of the Democratic Party, Obama was the first African-American president of the United States. He previously served as a U.S. senator from Illinois from 2005 to 2008 and as an Illinois state senator from 1997 to 2004. Outside of politics, Obama has published three bestselling books; Dreams from My Father (1995), The Audacity of Hope (2006) and A Promised Land (2020).[2]
@@ -29,6 +30,6 @@ Obama was born in Honolulu, Hawaii. After graduating from Columbia University in
 });
 
 const getPublicYouths = (): PublicYouth[] =>
-  [...Array(30)].fill(getPublicYouth());
+  [...Array(30)].map((v, i) => getPublicYouth(i));
 
 export { getDonation, getDonations, getPublicYouth, getPublicYouths };
