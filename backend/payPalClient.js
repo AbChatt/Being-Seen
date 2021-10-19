9587ec808b72332fca90359df80e643bb266d77c
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-19 10:15:22 -0400

    TEA4-53: clean up PayPal client

--- backend/payPalClient.js
+++ backend/payPalClient.js
@@ -1,62 +1,23 @@
-'use strict';
-
-/**
- * PayPal Node JS SDK dependency
- */
-import checkoutNodeJssdk from "@paypal/checkout-server-sdk";
+import payPalSdk from "@paypal/checkout-server-sdk";
 
 /**
  * Returns PayPal HTTP client instance with environment which has access
  * credentials context. This can be used invoke PayPal API's provided the
  * credentials have the access to do so.
  */
-function client() {
-    return new checkoutNodeJssdk.core.PayPalHttpClient(environment());
-}
+const client = () => new payPalSdk.core.PayPalHttpClient(environment());
 
 /**
  * Setting up and Returns PayPal SDK environment with PayPal Access credentials.
  * For demo purpose, we are using SandboxEnvironment. In production this will be
  * LiveEnvironment.
  */
-function environment() {
-    let clientId = process.env.PAYPAL_CLIENT_ID || '<<CLIENT-ID>>';
-    let clientSecret = process.env.PAYPAL_CLIENT_SECRET || '<<CLIENT-SECRET>>';
+const environment = () => {
+  const clientId =
+    "AXidlZhw-HrSH88Cl7DZnDKHKXICHElpSTshdoog9mSJORngN52503ugVpsUQNrJUfUDSp2jgAHoav6K";
+  const clientSecret =
+    "EJ9y6s2j3fRWhtIKQjlF8PIFQ6F3g8umkkbwoqDK-LIMytgc5z7IbSUQrxOPsvIs2SF7Yx0iYCI0qNYe";
+  return new payPalSdk.core.SandboxEnvironment(clientId, clientSecret);
+};
 
-    if (process.env.NODE_ENV === 'production') {
-        return new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret);
-     }
-
-    return new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
-}
-
-async function prettyPrint(jsonData, pre=""){
-    let pretty = "";
-    function capitalize(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-    }
-    for (let key in jsonData){
-        if (jsonData.hasOwnProperty(key)){
-            if (isNaN(key))
-              pretty += pre + capitalize(key) + ": ";
-            else
-              pretty += pre + (parseInt(key) + 1) + ": ";
-            if (typeof jsonData[key] === "object"){
-                pretty += "\n";
-                pretty += await prettyPrint(jsonData[key], pre + "\t");
-            }
-            else {
-                pretty += jsonData[key] + "\n";
-            }
-
-        }
-    }
-    return pretty;
-}
-
-
-
-//module.exports = {client: client, prettyPrint:prettyPrint};
-//export default { client: myclientobj, prettyPrint: myprettyprintobj };
-
-export {client, prettyPrint};
+export default client;
