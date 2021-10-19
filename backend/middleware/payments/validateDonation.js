2520b62987877024193fa7c9cf02d9de42c49f8f
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-19 17:13:09 -0400

    TEA4-53: create middleware to validate donation creation

---
+++ backend/middleware/payments/validateDonation.js
@@ -0,0 +1,52 @@
+import { StatusCodes } from "http-status-codes";
+import { createTextMessage } from "../../utils/defaultMessages.js";
+import userRoles from "../../utils/userRoles.js";
+import User from "../../models/User.js";
+
+// Middleware to validate whether parameters associated with the create donation
+// endpoint (donation amount, youth, donor) are present and valid
+const validateCreateDonation = (req, res, next) => {
+  console.log(req.body);
+
+  // Validate donation amount
+  const validAmounts = [5, 10, 25, 100];
+  if (!req.body.amount) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("Donation amount is empty"));
+  } else if (!validAmounts.includes(+req.body.amount)) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("Donation amount is invalid"));
+  }
+
+  // Validate youth username
+  if (!req.body.youth) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("Youth username is empty"));
+  } else if (
+    !(await User.exists({ username: req.body.username, role: userRoles.youth }))
+  ) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .send(createTextMessage("Youth account does not exist"));
+  }
+
+  // Validate donor username
+  if (!req.body.donor) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("Donor username is empty"));
+  } else if (
+    !(await User.exists({ username: req.body.username, role: userRoles.donor }))
+  ) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .send(createTextMessage("Donor account does not exist"));
+  }
+
+  next();
+};
+
+export { validateCreateDonation };
