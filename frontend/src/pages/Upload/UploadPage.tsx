e6559d2feaf3a7b13a61d8e79190e2dbe0f6d51f
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-22 14:20:57 -0400

    TEA4-44: basic reformatting and minor updates

--- frontend/src/pages/Upload/UploadPage.tsx
+++ frontend/src/pages/Upload/UploadPage.tsx
@@ -1,15 +1,20 @@
-import { useHistory } from "react-router-dom";
-import { decodeAuthToken } from "utils/authHelpers";
-import UserRoles from "utils/UserRoles";
-import Layout from "components/Layout";
 import React, { useState } from "react";
-import axiosBase from "utils/axiosBase";
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+
 import Box from "@mui/material/Box";
-import { toast, ToastContainer } from "react-toastify";
-import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
+import TextField from "@mui/material/TextField";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+import Layout from "components/Layout";
 import { getAuthHeader } from "utils/authHelpers";
+import { decodeAuthToken } from "utils/authHelpers";
+import handleResponseError from "utils/handleResponseError";
+import UserRoles from "utils/UserRoles";
+import axiosBase from "utils/axiosBase";
 
 // Render the profile page of the application. If a user is not logged in (or
 // is not a merchant), we redirect them to the homepage.
@@ -17,8 +22,8 @@ const UploadPage = () => {
   const history = useHistory();
   const account = decodeAuthToken();
   const [name, setName] = useState("");
-  const [pictureUrl, setPictureUrl] = useState("");
   const [price, setPrice] = useState("");
+  const [pictureUrl, setPictureUrl] = useState("");
   const [description, setDescription] = useState("");
 
   if (!account || !(account.role === UserRoles.merchant)) {
@@ -45,91 +50,83 @@ const UploadPage = () => {
         history.push("/profile");
       })
       .catch(({ response }) => {
-        // On failure, we determine whether the request itself was made. If so,
-        // we display the error message from the API to the merchant.
-        if (response) {
-          toast.error(response.data.message || "Unknown error");
-        } else {
-          toast.error("Request could not be made");
-        }
+        handleResponseError(response, toast);
       });
   };
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
   };
 
-  const handlePictureChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPictureUrl(event.target.value);
+  const handlePictureChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPictureUrl(e.target.value);
   };
 
-  const handleDescriptionChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setDescription(event.target.value);
+  const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(e.target.value);
   };
 
-  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPrice(event.target.value);
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice(e.target.value);
   };
 
   return (
     <Layout title="Upload Page">
-      <h1>Upload a new product</h1>
-      <Box noValidate component="form" onSubmit={handleUpload} sx={{ mt: 1 }}>
-        <ToastContainer theme="colored" />
-        <TextField
-          autoFocus
-          required
-          fullWidth
-          value={name}
-          onChange={handleNameChange}
-          autoComplete="name"
-          label="Name"
-          margin="normal"
-        />
-        <TextField
-          autoFocus
-          required
-          fullWidth
-          value={description}
-          onChange={handleDescriptionChange}
-          autoComplete="description"
-          label="Description"
-          margin="normal"
-        />
-        <TextField
-          autoFocus
-          required
-          fullWidth
-          value={price}
-          onChange={handlePriceChange}
-          autoComplete="price"
-          label="Price"
-          margin="normal"
-        />
-        <TextField
-          fullWidth
-          value={pictureUrl}
-          onChange={handlePictureChange}
-          label="Product Picture URL"
-          margin="normal"
-        />
-        {pictureUrl && (
-          <Box display="flex" justifyContent="center" mt={2}>
-            <Avatar src={pictureUrl} sx={{ width: 64, height: 64 }} />
-          </Box>
-        )}
-        <Button
-          fullWidth
-          size="large"
-          type="submit"
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Upload
-        </Button>
-      </Box>
+      <Container maxWidth="xl" sx={{ py: 5 }}>
+        <Typography gutterBottom variant="h4">
+          Upload a new product
+        </Typography>
+        <Box noValidate component="form" onSubmit={handleUpload} sx={{ mt: 1 }}>
+          <TextField
+            autoFocus
+            required
+            fullWidth
+            value={name}
+            onChange={handleNameChange}
+            label="Product Name"
+            margin="normal"
+          />
+          <TextField
+            required
+            fullWidth
+            value={description}
+            onChange={handleDescriptionChange}
+            label="Description"
+            margin="normal"
+          />
+          <TextField
+            required
+            fullWidth
+            value={price}
+            onChange={handlePriceChange}
+            autoComplete="price"
+            label="Price"
+            margin="normal"
+            type="number"
+          />
+          <TextField
+            fullWidth
+            value={pictureUrl}
+            onChange={handlePictureChange}
+            label="Product Picture URL"
+            margin="normal"
+          />
+          {pictureUrl && (
+            <Box display="flex" justifyContent="center" mt={2}>
+              <Avatar src={pictureUrl} sx={{ width: 64, height: 64 }} />
+            </Box>
+          )}
+          <Button
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Upload
+          </Button>
+        </Box>
+      </Container>
     </Layout>
   );
 };
