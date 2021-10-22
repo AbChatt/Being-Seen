d8b039c682f95a5aabeaa0464e3790d4d824c0ed
WilsonUTrash <luoweitan44@gmail.com>
2021-10-22 12:54:00 -0400

    TEA4-45: change the authToken from merchant to youth

--- frontend/src/pages/Store/StorePage.tsx
+++ frontend/src/pages/Store/StorePage.tsx
@@ -4,12 +4,12 @@ import UserRoles from "utils/UserRoles";
 import Layout from "components/Layout";
 
 // Render the store page of the application. If a user is not logged in (or does
-// not have the merchant role), we redirect them to the homepage.
+// not have the youth role), we redirect them to the homepage.
 const StorePage = () => {
   const history = useHistory();
   const account = decodeAuthToken();
 
-  if (!account || account.role !== UserRoles.merchant) {
+  if (!account || account.role !== UserRoles.youth) {
     history.push("/");
   }
 
