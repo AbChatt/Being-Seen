96944180f2ebd79df4878a55d8864af53460e174
WilsonUTrash <luoweitan44@gmail.com>
2021-10-22 12:57:16 -0400

    TEA4-45: Add rough component to Store page

--- frontend/src/pages/Store/StorePage.tsx
+++ frontend/src/pages/Store/StorePage.tsx
@@ -16,6 +16,32 @@ const StorePage = () => {
   return (
     <Layout title="Store">
       <h1>Store Page</h1>
+    <Layout title="Store" loading={loading}>
+      <ToastContainer theme="colored" />
+      <Container maxWidth="xl" sx={{ py: 5 }}>
+        <Box
+          mb={3}
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Typography variant="h4">Products</Typography>
+          <Button to="/upload" component={Link} variant="contained">
+            Upload new product
+          </Button>
+        </Box>
+        {products.length ? (
+          <Grid container spacing={2}>
+            {products.map((product, idx) => (
+              <Grid key={`p-${idx}`} item xs={12} sm={6} md={4} lg={3} xl={2}>
+                <ProductCard {...product} />
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <Typography> No products </Typography>
+        )}
+      </Container>
     </Layout>
   );
 };
