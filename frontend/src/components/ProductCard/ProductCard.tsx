fbe24dfda7dfa7bef49a36d4d2fafe270c3c0f87
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-21 22:14:05 -0400

    TEA4-43: use avatar for product image and clean up file

--- frontend/src/components/ProductCard/ProductCard.tsx
+++ frontend/src/components/ProductCard/ProductCard.tsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
+
+import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
+import Button from "@mui/material/Button";
+import Collapse from "@mui/material/Collapse";
+import Typography from "@mui/material/Typography";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
-import * as React from "react";
-import { styled } from "@mui/material/styles";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Collapse from "@mui/material/Collapse";
+import { styled } from "@mui/material/styles";
 import { Products } from "common/Types";
 
 interface ExpandMoreProps extends IconButtonProps {
@@ -30,18 +31,22 @@ export default function ProductCard({
   name,
   description,
   picture,
-  owner,
   price,
 }: Products) {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia component="img" height="140" image={picture} alt={name} />
+    <Card>
+      <Avatar
+        src={picture}
+        variant="square"
+        sx={{ height: 200, width: "100%" }}
+        alt={name}
+      />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {name}
@@ -53,12 +58,7 @@ export default function ProductCard({
       <CardActions disableSpacing>
         <Button size="small">Edit</Button>
         <Button size="small">Delete</Button>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
+        <ExpandMore expand={expanded} onClick={handleExpandClick}>
           <ExpandMoreIcon />
         </ExpandMore>
       </CardActions>
