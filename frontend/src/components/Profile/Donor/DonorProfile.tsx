18734e7b7227e49811014d1ae0a47145cccb1e06
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-27 16:23:28 -0400

    TEA4-62: create donor donation table UI

--- frontend/src/components/Profile/Donor/DonorProfile.tsx
+++ frontend/src/components/Profile/Donor/DonorProfile.tsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from "react";
+
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+import { Donor } from "common/Types";
+import { decodeAuthToken } from "utils/authHelpers";
+import DonationCard from "components/Card/Donation";
 import Layout from "components/Layout";
 
-const DonorProfile = () => {
+const YouthProfile = () => {
+  const account = decodeAuthToken();
+  const [loading] = useState(true);
+  const [donor] = useState<Donor | null>(null);
+
+  useEffect(() => {}, [account?.username]);
+
   return (
-    <Layout title="Donor Profile">
-      <h1>Donor profile page</h1>
+    <Layout title="Donor Profile" loading={loading}>
+      <Container maxWidth="xl" sx={{ py: 5 }}>
+        {donor ? (
+          <>
+            <Typography variant="h4" mb={4}>
+              Welcome {donor.name}!
+            </Typography>
+            {donor.donations.length === 0 ? (
+              <Typography>No donations yet!</Typography>
+            ) : (
+              <DonationCard donations={donor.donations} />
+            )}
+          </>
+        ) : (
+          <Typography variant="h4">
+            Issue loading {account?.username}
+          </Typography>
+        )}
+      </Container>
     </Layout>
   );
 };
 
-export default DonorProfile;
+export default YouthProfile;
