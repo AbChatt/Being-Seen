bfa22446029b74db3a7b8627a0afe88d82bcfcde
Joshua Daniel <joshuajadaniel@gmail.com>
2021-10-28 14:41:18 -0400

    TEA4-70: stylistic changes on backend code for youth update

--- backend/middleware/update/validateUpdateYouth.js
+++ backend/middleware/update/validateUpdateYouth.js
@@ -3,30 +3,15 @@ import { StatusCodes } from "http-status-codes";
 import { createTextMessage } from "../../utils/defaultMessages.js";
 
 // Middleware to validate whether parameters associated with the validate
-// update endpoint (youth) are present and valid
+// update endpoint (name) are present and valid
 const validateUpdateYouth = async (req, res, next) => {
   console.log(req.body);
 
-  // Validate input feild
+  // Validate input name
   if (!req.body.name) {
     return res
       .status(StatusCodes.BAD_REQUEST)
-      .send(createTextMessage("youth name is empty"));
-  }
-  if (!req.body.profile_picture) {
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .send(createTextMessage("profile picture is empty"));
-  }
-  if (!req.body.story) {
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .send(createTextMessage("stroy is empty"));
-  }
-  if (!req.body.saving_plan) {
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .send(createTextMessage("saving_plan is empty"));
+      .send(createTextMessage("Name field cannot be empty"));
   }
 
   next();
