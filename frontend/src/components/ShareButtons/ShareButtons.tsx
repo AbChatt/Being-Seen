c905e5b86d2b86feb1be1f24b255159220c17d07
Joshua Daniel <joshuajadaniel@gmail.com>
2021-11-01 21:31:01 -0400

    TEA4-81: swap share buttons for dialog component

--- frontend/src/components/ShareButtons/ShareButtons.tsx
+++ frontend/src/components/ShareButtons/ShareButtons.tsx
@@ -1,58 +1,86 @@
+import { useState } from "react";
+
 import {
-  FacebookShareButton,
-  LinkedinShareButton,
+  EmailShareButton,
   RedditShareButton,
   TwitterShareButton,
   WeiboShareButton,
   WhatsappShareButton,
-  FacebookIcon,
-  LinkedinIcon,
+  EmailIcon,
   TwitterIcon,
   RedditIcon,
   WeiboIcon,
   WhatsappIcon,
 } from "react-share";
 
-import "./style.css";
+import Fab from "@mui/material/Fab";
+import Dialog from "@mui/material/Dialog";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import ShareIcon from "@mui/icons-material/Share";
+import Box from "@mui/material/Box";
+
+import styles from "./ShareButton.module.scss";
 
 interface ShareButtonsProps {
   shareUrl: string;
 }
 
 const ShareButtons = ({ shareUrl }: ShareButtonsProps) => {
+  const [open, setOpen] = useState(false);
+  const openDialog = () => setOpen(true);
+  const closeDialog = () => setOpen(false);
+
   return (
-    <div className="share-btn-container">
-      <p>
-        <TwitterShareButton url={shareUrl}>
-          <TwitterIcon size={45} round={true} />
-        </TwitterShareButton>
-      </p>
-      <p>
-        <FacebookShareButton url={shareUrl}>
-          <FacebookIcon size={45} round={true} />
-        </FacebookShareButton>
-      </p>
-      <p>
-        <LinkedinShareButton url={shareUrl}>
-          <LinkedinIcon size={45} round={true} />
-        </LinkedinShareButton>
-      </p>
-      <p>
-        <RedditShareButton url={shareUrl}>
-          <RedditIcon size={45} round={true} />
-        </RedditShareButton>
-      </p>
-      <p>
-        <WhatsappShareButton url={shareUrl}>
-          <WhatsappIcon size={45} round={true} />
-        </WhatsappShareButton>
-      </p>
-      <p>
-        <WeiboShareButton url={shareUrl}>
-          <WeiboIcon size={45} round={true} />
-        </WeiboShareButton>
-      </p>
-    </div>
+    <>
+      <Dialog onClose={closeDialog} open={open} maxWidth="sm" fullWidth>
+        <Typography align="center" variant="h5" mt={5}>
+          Share my profile
+        </Typography>
+        <Box my={5} px={2} className={styles.buttonContainer}>
+          <Button component="span" color="inherit">
+            <TwitterShareButton url={shareUrl}>
+              <TwitterIcon size={50} round />
+              <Typography>Twitter</Typography>
+            </TwitterShareButton>
+          </Button>
+          <Button component="span" color="inherit">
+            <RedditShareButton url={shareUrl}>
+              <RedditIcon size={50} round />
+              <Typography>Reddit</Typography>
+            </RedditShareButton>
+          </Button>
+          <Button component="span" color="inherit">
+            <WeiboShareButton url={shareUrl}>
+              <WeiboIcon size={50} round />
+              <Typography>Weibo</Typography>
+            </WeiboShareButton>
+          </Button>
+          <Button component="span" color="inherit">
+            <WhatsappShareButton url={shareUrl}>
+              <WhatsappIcon size={50} round />
+              <Typography>WhatsApp</Typography>
+            </WhatsappShareButton>
+          </Button>
+          <Button component="span" color="inherit">
+            <EmailShareButton url={shareUrl}>
+              <EmailIcon size={50} round />
+              <Typography>Email</Typography>
+            </EmailShareButton>
+          </Button>
+        </Box>
+      </Dialog>
+      <Tooltip title="Share" placement="left">
+        <Fab
+          color="primary"
+          onClick={openDialog}
+          className={styles.shareButton}
+        >
+          <ShareIcon />
+        </Fab>
+      </Tooltip>
+    </>
   );
 };
 
