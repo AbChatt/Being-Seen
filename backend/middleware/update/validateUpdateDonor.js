a2577b6949df8d055f21e8b1b0e54c4bc540f51d
Tony-beeper <tyx1999120755@gmail.com>
2021-11-03 22:47:08 -0400

    TEA4-77:added middleware to authenticate front-end input

---
+++ backend/middleware/update/validateUpdateDonor.js
@@ -0,0 +1,38 @@
+import { StatusCodes } from "http-status-codes";
+
+import { createTextMessage } from "../../utils/defaultMessages.js";
+
+// Middleware to validate whether parameters associated with the validate
+// update endpoint (name) are present and valid
+const validateUpdateDonor = async (req, res, next) => {
+  console.log(req.body.name);
+  console.log(req.body.organization);
+
+  // Validate input name
+  if (!req.body.name) {
+    console.log(req.body.name);
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("Name field cannot be empty"));
+  }
+
+  // Validate input organization
+
+  if (!req.body.organization) {
+    console.log(req.body.organization);
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("organzation field cannot be empty"));
+  }
+
+  // Validate pictureUrl
+  if (!req.body.profile_picture) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send(createTextMessage("profile picture field cannot be empty"));
+  }
+
+  next();
+};
+
+export default validateUpdateDonor;
