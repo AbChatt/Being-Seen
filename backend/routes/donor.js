d91ce1b68c8c39f6596e713bee39ef85c9f35def
Joshua Daniel <joshuajadaniel@gmail.com>
2021-11-10 16:24:36 -0500

    TEA4-84: clean up route comments and extract body values into variables

--- backend/routes/donor.js
+++ backend/routes/donor.js
@@ -7,13 +7,12 @@ import verifyAuthHeader from "../middleware/security/verifyAuthHeader.js";
 import validateUpdateDonor from "../middleware/update/validateUpdateDonor.js";
 
 import { decodeUserToken, createUserToken } from "../utils/jwtHelpers.js";
+import { parseRetrievedDonor } from "../utils/parseModelObjects.js";
 import { createTextMessage } from "../utils/defaultMessages.js";
 import { createJwtMessage } from "../utils/defaultMessages.js";
 import userRoles from "../utils/userRoles.js";
 
 import Donor from "../models/Donor.js";
-import Youth from "../models/Youth.js";
-import Donation from "../models/Donation.js";
 import User from "../models/User.js";
 
 const router = express.Router();
@@ -21,26 +20,37 @@ const router = express.Router();
 // api/v1/user/donor/signup
 router.use("/signup", [validateUserSignup, validateDonorSignup]);
 router.post("/signup", async (req, res) => {
+  // Required fields
+  const name = req.body.name;
+  const username = req.body.username;
+  const dateOfBirth = req.body.date_of_birth;
+  const password = req.body.password;
+
+  // Optional fields
+  const organization = req.body.organization;
+  const profilePicture = req.body.profile_picture;
+  const anonymize = req.body.anonymize;
+
   const newUser = new User({
     role: userRoles.donor,
-    username: req.body.username,
-    password: req.body.password,
+    username: username,
+    password: password,
   });
 
   const newDonor = new Donor({
-    name: req.body.name,
-    username: req.body.username,
-    date_of_birth: req.body.date_of_birth,
-    organization: req.body.organization || "None",
-    profile_picture: req.body.profile_picture || "#",
-    display_name: Boolean(req.body.anonymize) ? "Anonymous" : req.body.name,
-    anonymize: Boolean(req.body.anonymize),
+    name: name,
+    username: username,
+    date_of_birth: dateOfBirth,
+    organization: organization || "None",
+    profile_picture: profilePicture || "#",
+    display_name: Boolean(anonymize) ? "Anonymous" : name,
+    anonymize: Boolean(anonymize),
   });
 
   try {
     await newUser.save();
     await newDonor.save();
-    const jwtToken = createUserToken(req.body.username, userRoles.donor);
+    const jwtToken = createUserToken(username, userRoles.donor);
     return res.status(StatusCodes.CREATED).send(createJwtMessage(jwtToken));
   } catch (err) {
     console.log(err);
@@ -50,46 +60,17 @@ router.post("/signup", async (req, res) => {
   }
 });
 
-// Method to generate the API returned donor object based on a retrieved
-// donor object from database
-const parseRetrievedDonor = async (retrievedDonor) => {
-  const rawDonations = await Donation.find({
-    donor: retrievedDonor.username,
-  });
-
-  const parsedDonations = await Promise.all(
-    rawDonations.map(async (donation) => {
-      const retrievedYouth = await Youth.findOne({
-        username: donation.youth,
-      });
-
-      return {
-        donor: retrievedDonor.display_name,
-        youth: retrievedYouth.name,
-        amount: donation.amount,
-        date: donation.date,
-      };
-    })
-  );
-
-  return {
-    name: retrievedDonor.name,
-    username: retrievedDonor.username,
-    dateOfBirth: retrievedDonor.date_of_birth,
-    profilePicture: retrievedDonor.profile_picture,
-    organization: retrievedDonor.organization,
-    anonymize: retrievedDonor.anonymize,
-    donations: parsedDonations,
-  };
-};
-
 // api/v1/user/donor/private
 router.use("/private", verifyAuthHeader(userRoles.donor));
 router.post("/private", async (req, res) => {
-  const decoded = decodeUserToken(req.headers.authorization);
+  const decodedDonor = decodeUserToken(req.headers.authorization);
+
   try {
-    const retrievedDonor = await Donor.findOne({ username: decoded.username });
+    const retrievedDonor = await Donor.findOne({
+      username: decodedDonor.username,
+    });
     const parsedDonor = await parseRetrievedDonor(retrievedDonor);
+
     return res.send(parsedDonor);
   } catch (err) {
     console.log(err);
@@ -101,28 +82,33 @@ router.post("/private", async (req, res) => {
 
 // api/v1/user/donor/update
 router.use("/update", [verifyAuthHeader(userRoles.donor), validateUpdateDonor]);
-router.put("/update", async (req, res) => {
-  // Get require JWT token that include donor username
-  const decoded = decodeUserToken(req.headers.authorization);
-  const donorUsername = decoded.username;
+router.patch("/update", async (req, res) => {
+  const decodedDonor = decodeUserToken(req.headers.authorization);
+
+  // Required fields
+  const name = req.body.name;
+
+  // Optional fields
+  const organization = req.body.organization;
+  const profilePicture = req.body.profile_picture;
+  const anonymize = req.body.anonymize;
 
   try {
-    // Find a donor and update them
-    await Donor.findOneAndUpdate(
-      { username: donorUsername },
+    await Donor.updateOne(
+      { username: decodedDonor.username },
       {
-        name: req.body.name,
-        organization: req.body.organization || "None",
-        profile_picture: req.body.profile_picture || "#",
-        display_name: Boolean(req.body.anonymize) ? "Anonymous" : req.body.name,
-        anonymize: Boolean(req.body.anonymize),
+        name: name,
+        organization: organization || "None",
+        profile_picture: profilePicture || "#",
+        display_name: Boolean(anonymize) ? "Anonymous" : name,
+        anonymize: Boolean(anonymize),
       }
     );
     return res.send(createTextMessage("Successfully updated your profile"));
   } catch (err) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send(createTextMessage("Error update profile"));
+      .send(createTextMessage("Error updating your profile"));
   }
 });
 
