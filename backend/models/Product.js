8b22ffbd42c2cd31d3237bbc7d1df0cdff490b0a
WilsonUTrash <luoweitan44@gmail.com>
2021-11-15 14:52:58 -0500

    TEA4-92: update product schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -21,6 +21,11 @@ const ProductSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+
+  category: {
+    type: String,
+    required: true,
+  },
 });
 
 export default mongoose.model("products", ProductSchema);
